396790058f79a5efc2fb8d4adea5a9400a537ade
codyowl <codyowl@gmail.com>
2017-01-04 14:35:39 +0530

    Module structure done ! :baby_bottle:

---
+++ transformers/Ui/UiTransform.py
@@ -0,0 +1,4 @@
+class BootstrapTransform:
+	def create_template(self, template_name, title=None):
+		self.template_name = template_name
+		self.title = title
---
+++ transformers/Ui/__init__.py
@@ -0,0 +1,1 @@
+from UiTransform import BootstrapTransform
