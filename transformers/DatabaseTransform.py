43326ee3f3c8c59adc6f1e6ccfebcf7eaeb4fa0c
codyowl <codyowl@gmail.com>
2017-02-16 17:26:19 +0530

    Restructed and table name getter added :rocket:

--- transformers/DatabaseTransform.py
+++ transformers/DatabaseTransform.py
@@ -7,11 +7,22 @@ from collections import OrderedDict
 
 
 class MysqlTransform:
+	def __init__(self, username, password, database_name):
+		self.username = username
+		self.password = password
+		self.database_name = database_name
+		self.connector = MySQLdb.connect(user=username, passwd=password, db=db)
+		self.cursor = self.connector.cursor()
+
 	def transform_to(self, database_type):
 		self.database_type = database_type
-	def mysql_database_name(self, database_name):
-		self.database_name = database_name	
-	def mysql_username(self, user_name):
-		self.username = username
-	def mysql_password(self, password):
-		self.password = password		
+		return self.database_type.lower()
+
+	def table_getter(self, database_name=self.database_name):
+		query = "SHOW TABLES;"
+		self.cursor.execute(query)
+		self.fetcher = self.cursor.fetchall()
+		return self.fetcher
+			
+	def transformer(self):
+		query = ""	
